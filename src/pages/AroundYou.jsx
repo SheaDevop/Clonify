ba9221a0d7470642a5336859a985f21521cf5ea2
SHEA <nacho.dieguez18@gmail.com>
2023-04-03 13:13:34 -0300

    searchbar page and fixed some errors

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -28,7 +28,7 @@ const AroundYou = () => {
 				Around You <span className='font-black'>{country}</span>
 			</h2>
 			<div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-				{data?.tracks?.map((song, i) => (
+				{data?.map((song, i) => (
 					<SongCard
 						key={song.key}
 						song={song}
